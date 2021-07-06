4212318162c3cbcf50f6ffc0c751ddf7ac09d9d1
Your Name <you@example.com>
2021-07-06 17:47:39 +0400

    comment

---
+++ GrEaEa.js
@@ -0,0 +1,69 @@
+class GrEaEa {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+
+    }
+    chooseCell(character) {
+
+        var found = [];
+        for (var i in this.directions) {
+
+            var x = this.directions[i][0];
+            var y = this.directions[i][1];
+            if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                if (matrix[y][x] == character) {
+                    found.push(this.directions[i]);
+                }
+            }
+
+        }
+        return found;
+    }
+
+    move() {
+        var emptyCells = this.chooseCell(0)
+        var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+
+        if (newCell) {
+            console.log(newCell)
+            var newX = newCell[0]
+            var newY = newCell[1]
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+        }
+    }
+
+    eat() {
+        var emptyCells = this.chooseCell(3)
+        var newCell = emptyCells[Math.floor(Math.random() * emptyCells.length)]
+
+        if (newCell) {
+            var newX = newCell[0]
+            var newY = newCell[1]
+
+            matrix[newY][newX] = matrix[this.y][this.x]
+            matrix[this.y][this.x] = 0
+            this.x = newX
+            this.y = newY
+            for (var i in PredatorArr) {
+                if (newX == PredatorArr[i].x && newY == PredatorArr[i].y) {
+                    PredatorArr.splice(i, 1)
+                    break
+                }
+            }
+        }
+    }
+}
